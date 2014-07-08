72948f8e6ed78be00cfff1a75fb9eacfa894343f
Alex Peto <tricksternole@gmail.com>
2014-07-08 00:54:38 -0400

    start windgate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Windgate::Application.config.secret_key_base = '531697bb7e850ea13cb37277c25daec7013ab3f223244e6472b28cfa75f41605dd6cf69d96c40a1f98956004bb8c05260711812810485653109c4c4194d65c6d'
